c765579c245a7da16be50af5455ca4fcb6b3366d
haronpaulson <93569110+haronpaulson@users.noreply.github.com>
2023-04-25 10:04:52 +0530

    Update navigation.js

--- components/navigation.js
+++ components/navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 function Navigation () {
     return(
         <div>
-            <img src= "anitechlogo.png" alt="companylogo" className=" w-40 mb-2 ml-52"/>
+            
             <h1 className=" font-semibold mb-2 text-2xl pl-10 ">Cost Estimate for audiometric assessment</h1>
         </div>
     )
